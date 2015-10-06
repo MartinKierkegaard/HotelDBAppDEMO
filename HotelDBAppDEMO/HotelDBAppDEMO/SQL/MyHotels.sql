e6421f9b4abcc034712e739c4fe6893f196f1e35
Martin Kierkegaard <martinkierkegaard@yahoo.com>
2015-10-06 10:54:53 +0200

    Added a sql listing hotels

---
+++ HotelDBAppDEMO/HotelDBAppDEMO/SQL/MyHotels.sql
@@ -0,0 +1,15 @@
+﻿select *
+from dbo.Hotel
+
+SELECT *
+from dbo.Room
+where Hotel_No = 200
+
+SELECT h.Name, r.Room_No, r.Price, r.Types
+FROM dbo.Hotel h
+Inner join
+dbo.Room r
+ON h.Hotel_No = r.Hotel_No
+WHERE h.Hotel_No = 200
+
+
