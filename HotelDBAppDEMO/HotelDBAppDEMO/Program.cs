d56c2bb726bbfccc0c04b870ee8666b23db654a4
Martin Kierkegaard <martinkierkegaard@yahoo.com>
2015-10-06 10:54:28 +0200

    Add the Legoland Hotel

--- HotelDBAppDEMO/HotelDBAppDEMO/Program.cs
+++ HotelDBAppDEMO/HotelDBAppDEMO/Program.cs
@@ -6,10 +6,46 @@ using System.Threading.Tasks;
 
 namespace HotelDBAppDEMO
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
+
+            using (var db = new HotelContext())
+            {
+                List<Room> LegolandRooms = new List<Room>()
+                {
+                    new Room() {Hotel_No = 200, Room_No = 1, Price = 200, Types = "D"},
+                    new Room() {Hotel_No = 200, Room_No = 2, Price = 250, Types = "S"},
+                    new Room() {Hotel_No = 200, Room_No = 3, Price = 400, Types = "D"},
+                    new Room() {Hotel_No = 200, Room_No = 4, Price = 500, Types = "F"},
+                };
+
+                Hotel MyNewHotel = new Hotel()
+                {
+                    Hotel_No = 300,
+                    Name = "LegolandHotel",
+                    Address = "Legoland vej 1",
+                    Room = LegolandRooms
+                };
+
+                db.Hotel.Add(MyNewHotel);
+
+                db.SaveChanges();
+
+                var hotellist = from h in db.Hotel
+                    where h.Hotel_No == 200
+                    select h;
+
+                foreach (var item in hotellist)
+                {
+                    Console.WriteLine(item.Name);
+                }
+
+
+                Console.ReadLine();
+
+            }
         }
     }
 }
