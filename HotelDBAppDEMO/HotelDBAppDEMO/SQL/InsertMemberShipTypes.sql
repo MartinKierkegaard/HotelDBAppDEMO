e6b286e7c4fbb4f71c330b1e42285da3e3821db0
Martin Kierkegaard <martinkierkegaard@yahoo.com>
2015-10-09 09:50:30 +0200

    Added sql for insert into membershipType

---
+++ HotelDBAppDEMO/HotelDBAppDEMO/SQL/InsertMemberShipTypes.sql
@@ -0,0 +1,24 @@
+﻿--Gold
+INSERT INTO [dbo].[MemberShipType]
+           ([GuestRating]
+           ,[Discount]
+           ,[SumBookings])
+     VALUES           
+	 (1 ,10 ,5)
+GO
+--Silver
+INSERT INTO [dbo].[MemberShipType]
+           ([GuestRating]
+           ,[Discount]
+           ,[SumBookings])
+     VALUES           
+	 (2 ,3 ,5)
+GO
+--Bronze
+INSERT INTO [dbo].[MemberShipType]
+           ([GuestRating]
+           ,[Discount]
+           ,[SumBookings])
+     VALUES           
+	 (3 ,2 ,2)
+GO
